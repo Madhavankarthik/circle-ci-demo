7de02e308d7b9d5f0c9fa71711397fa36b21da1c
Karthik Natarajan <madhavankarthi2010@gmail.com>
2022-05-25 19:47:09 +0530

    mend
    [Karthik] - | Adding wait to test

--- test/unit/test_my_sum.py
+++ test/unit/test_my_sum.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 from src.my_sum import my_sum
 
@@ -7,49 +8,59 @@ class TestSum(unittest.TestCase):
     def test_list_int(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_1(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_2(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_3(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_4(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_5(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_6(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_7(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_8(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
 
     def test_list_int_9(self):
         data = [1, 2, 3]
         result = my_sum(data)
+        time.sleep(10)
         self.assertEqual(result, 6)
