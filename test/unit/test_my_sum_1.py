9d7f6c730b8ed42003f41a7156f5eba2eb7d8612
Karthik Natarajan <madhavankarthi2010@gmail.com>
2022-05-25 19:40:43 +0530

    [Karthik] - | Adding multiple test

---
+++ test/unit/test_my_sum_1.py
@@ -0,0 +1,55 @@
+import unittest
+
+from src.my_sum import my_sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_1(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_2(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_3(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_4(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_5(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_6(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_7(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_8(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_9(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
